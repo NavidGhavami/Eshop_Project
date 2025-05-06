8ad59cb8ad961e48069c3801e7fdbdc496bcd903
Navid Ghavami <navid_ghavami@outlook.com>
2025-05-06 19:43:16 +0330

    Toast Notification has been added to Project.

--- ServiceHost/Controllers/AccountController.cs
+++ ServiceHost/Controllers/AccountController.cs
@@ -37,9 +37,8 @@ namespace ServiceHost.Controllers
         [HttpPost("register"), ValidateAntiForgeryToken]
         public async Task<IActionResult> RegisterUser(RegisterUserDto register)
         {
-
-            if (ModelState.IsValid)
-            {
+            //if (ModelState.IsValid)
+            //{
                 var result = await _userService.RegisterUser(register);
 
                 switch (result)
@@ -47,17 +46,18 @@ namespace ServiceHost.Controllers
                     case RegisterUserResult.MobileExists:
                         TempData[WarningMessage] = $"شماره همراه : {register.Mobile} تکراری می باشد.";
                         ModelState.AddModelError("Mobile", "شماره همراه تکراری می باشد.");
-                        break;
-                    case RegisterUserResult.Error:
+                    //break;
+                        return RedirectToAction("Index", "Home");
+                case RegisterUserResult.Error:
                         TempData[ErrorMessage] = "در ثبت اطلاعات خطایی رخ داد. لطفا دوباره تلاش نمایید.";
                         break;
                     case RegisterUserResult.Success:
                         TempData[InfoMessage] = $"کد تایید، جهت فعالسازی حساب کاربری به شماره همراه {register.Mobile} ارسال گردید.";
-                        return RedirectToAction("ActivateMobile", "Account");
-                }
+                    //return RedirectToAction("ActivateMobile", "Account");
+                        return RedirectToAction("Index", "Home");
 
-       
             }
+            //}
             return View(register);
         }
 
